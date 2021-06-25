be62e5a38c82141f10d09e37a5d14fead7c91e94
hammy3502 <hammy275@gmail.com>
2021-06-24 22:05:54 -0400

    Initial Commit (Not Working)

---
+++ src/net/blf02/dungeondash/DungeonDash.java
@@ -0,0 +1,16 @@
+package net.blf02.dungeondash;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class DungeonDash extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        super.onEnable();
+    }
+
+    @Override
+    public void onDisable() {
+        super.onDisable();
+    }
+}
